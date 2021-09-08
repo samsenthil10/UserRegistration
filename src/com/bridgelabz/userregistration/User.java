58fe4612cd77cf856bac3cbc4ab21df6e3eb5d9c
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-08 07:44:08 +0530

    Modified: To validate email for given test cases

--- src/com/bridgelabz/userregistration/User.java
+++ src/com/bridgelabz/userregistration/User.java
@@ -28,6 +28,50 @@ public class User {
 		return password.matches(regularExpression);
 	}
 	
+	public static void myTestCases() {
+		
+		String[] testCases = {
+				"abc.xyz@bridgelabz.co.in",
+				"abc@yahoo.com,",
+				"abc-100@yahoo.com,",
+				"abc.100@yahoo.com",
+				"abc111@abc.com,",
+				"abc-100@abc.net,",
+				"abc.100@abc.com.au",
+				"abc@1.com,",
+				"abc@gmail.com.com",
+				"abc+100@gmail.com",
+
+				"abc",
+				"abc@.com.my",
+				"abc123@gmail.a",
+				"abc123@.com",
+				"abc123@.com.com",
+				".abc@abc.com",
+				"abc()*@gmail.com",
+				"abc@%*.com",
+				"abc..2002@gmail.com",
+				"abc.@gmail.com",
+				"abc@abc@gmail.com",
+				"abc@gmail.com.1a",
+				"abc@gmail.com.aa.au"
+		}; 
+		
+		for(int index = 0;index < testCases.length;index++) {
+			String email = testCases[index];
+			boolean result = checkEmail(email);
+			if(result == true) {
+				System.out.println(testCases[index]+" : Valid Email!");
+				System.out.println();
+			}
+			else {
+				System.out.println(testCases[index]+" : Invalid Email!");
+				System.out.println();
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
@@ -83,7 +127,11 @@ public class User {
 				System.out.println("Invalid Password!");
 
 		}while(!checkPassword(password));
-
+		
+		System.out.println();
+		
+		myTestCases();
+		
 		scanner.close();
 	}
 }
