33ffc9cb5fcc3ee42579f8931f68e054cc8f9ea6
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-08 06:16:12 +0530

    Added: To validate and accept email

--- src/com/bridgelabz/userregistration/User.java
+++ src/com/bridgelabz/userregistration/User.java
@@ -9,13 +9,19 @@ public class User {
 		String regularExpression = "[A-Z]+[a-zA-Z\\ ]{2,}";
 		return firstName.matches(regularExpression);
 	} 
+	
+	public static boolean checkEmail(String email) {
+
+		String finalRegularExpressionToCheckEmail = "((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$";
+		return email.matches(finalRegularExpressionToCheckEmail);
+	}
 
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("----------Welcome to User Registration Portal----------");
 		System.out.println();
-		String firstName, lastName;
+		String firstName, lastName , email;
 		do{
 			System.out.print("Enter your First Name (*First letter in block and minimum 3 characters): ");
 			firstName = scanner.nextLine();
@@ -31,6 +37,14 @@ public class User {
 				System.out.println("Invalid Last Name!");
 			
 		}while(!checkName(lastName));
+		
+		do{
+			System.out.print("Enter your Email: ");
+			email = scanner.nextLine();
+			if(!checkEmail(email))
+				System.out.println("Invalid Email!");
+			
+		}while(!checkEmail(email));
 		
 		scanner.close();
 	}
